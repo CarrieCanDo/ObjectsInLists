74588a8c836341883a545612f8e886d3ead8c030
CarrieCanDo <carrie.a.wright@outlook.com>
2024-07-25 22:23:30 -0400

    Committing final assign

---
+++ src/main/java/org/example/BookMain.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BookMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Book> books = new ArrayList<>();
+
+        while (true) {
+            System.out.print("Enter the title of the book: ");
+            String title = scanner.nextLine();
+            if (title.isEmpty()) {
+                break;
+            }
+
+            System.out.print("Enter the number of pages: ");
+            int pages = Integer.parseInt(scanner.nextLine());
+
+            System.out.print("Enter the publication year: ");
+            int publicationYear = Integer.parseInt(scanner.nextLine());
+
+            books.add(new Book(title, pages, publicationYear));
+        }
+
+        System.out.print("What information will be printed? ");
+        String choice = scanner.nextLine();
+
+        if (choice.equals("everything")) {
+            for (Book book : books) {
+                System.out.println(book);
+            }
+        } else if (choice.equals("name")) {
+            for (Book book : books) {
+                System.out.println(book.getTitle());
+            }
+        }
+    }
+}
+
